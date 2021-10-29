b8e05fe4f055a84b2aeff040db2646041f672676
Anatol Panasiuk <anatolpanasiuk@MacBook.local>
2021-10-29 17:53:52 +0200

    new write functions and constructors to header and supported

---
+++ src/frame/tests/stringMultiMaps.go
@@ -0,0 +1,17 @@
+package main
+
+func testReadStringMultiMap() {
+	var m = map[string][]string{
+		"GOLang": {
+			"is", "super", "awesome!",
+		},
+		"Pets": {
+			"cat", "dog",
+		},
+	}
+	m.
+}
+
+func main() {
+
+}
