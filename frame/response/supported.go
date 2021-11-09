b0e7534db10044917ffa8a735271e453b035d0e9
Hubert Grochowski <hubert.grochowski21@gmail.com>
2021-11-09 10:37:13 +0100

    Comment change.

--- frame/response/supported.go
+++ frame/response/supported.go
@@ -1,7 +1,6 @@
 // Package response implements functions and types used for handling
 // all types of CQL binary protocol responses.
 // Reading from buffer is done in Big Endian order.
-// Response consists of frame.Header and body.
 
 package response
 
