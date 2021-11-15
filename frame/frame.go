89097a5cf9cf0efc77995c84fe5b4acd943045d3
Michal-Leszczynski <2000michal@wp.pl>
2021-11-15 23:11:33 +0100

    frame: added Authenticate Challenge and Event responses and Prepare and Query requests. Added necessary functions for reading and writing generic cql types. Minor code format and comments. Tests are yet to be added.

--- frame/frame.go
+++ frame/frame.go
@@ -27,6 +27,20 @@ func WriteInt(i Int, b *bytes.Buffer) {
 	})
 }
 
+// WriteLong writes single Long to the buffer.
+func WriteLong(l Long, b *bytes.Buffer) {
+	b.Write([]byte{
+		byte(l >> 56),
+		byte(l >> 48),
+		byte(l >> 40),
+		byte(l >> 32),
+		byte(l >> 24),
+		byte(l >> 16),
+		byte(l >> 8),
+		byte(l),
+	})
+}
+
 // WriteBytes writes Bytes to the buffer.
 // If Bytes is nil then writes -1 to the buffer.
 func WriteBytes(t Bytes, b *bytes.Buffer) {
@@ -43,10 +57,35 @@ func WriteBytes(t Bytes, b *bytes.Buffer) {
 	}
 }
 
+// WriteValue writes Value to the buffer.
+func WriteValue(v Value, b *bytes.Buffer) {
+	// Writes length of the value.
+	WriteInt(v.N, b)
+	// Writes value's body if there is any.
+	if v.N > 0 {
+		b.Write(v.Bytes)
+	}
+}
+
+// WriteInet writes Inet to the buffer.
+func WriteInet(i Inet, b *bytes.Buffer) {
+	// Writes length of the IP address.
+	WriteByte(Byte(len(i.IP)), b)
+	b.Write(i.IP)
+	WriteInt(i.Port, b)
+}
+
 // WriteString writes single string to the buffer.
 func WriteString(s string, b *bytes.Buffer) {
 	// Writes length of the string.
 	WriteShort(Short(len(s)), b)
+	b.WriteString(s)
+}
+
+// WriteLongString writes single long string to the buffer.
+func WriteLongString(s string, b *bytes.Buffer) {
+	// Writes length of the long string.
+	WriteInt(Int(len(s)), b)
 	b.WriteString(s)
 }
 
@@ -107,8 +146,22 @@ func ReadInt(b *bytes.Buffer) Int {
 		Int(tmp[3])
 }
 
+// ReadLong reads and returns Long from the buffer.
+func ReadLong(b *bytes.Buffer) Long {
+	tmp := [8]byte{0, 0, 0, 0, 0, 0, 0, 0}
+	_, _ = b.Read(tmp[:])
+	return Long(tmp[0])<<56 |
+		Long(tmp[1])<<48 |
+		Long(tmp[2])<<40 |
+		Long(tmp[3])<<32 |
+		Long(tmp[4])<<24 |
+		Long(tmp[5])<<16 |
+		Long(tmp[6])<<8 |
+		Long(tmp[7])
+}
+
 // ReadBytes reads Bytes from the buffer.
-// If read bytes length is negative returns nil.
+// If read Bytes length is negative returns nil.
 func ReadBytes(b *bytes.Buffer) Bytes {
 	// Reads length of the Bytes.
 	n := ReadInt(b)
@@ -121,6 +174,40 @@ func ReadBytes(b *bytes.Buffer) Bytes {
 		out = append(out, ReadByte(b))
 	}
 	return out
+}
+
+// ReadValue reads and return Value from the buffer.
+// Length equal to -1 represents null.
+// Length equal to -2 represents not set.
+func ReadValue(b *bytes.Buffer) Value {
+	// Reads length od the value.
+	n := ReadInt(b)
+	// Checks for valid length.
+	if n < -2 {
+		panic(invalidValueLength)
+	}
+	// Reads value's body if there is any.
+	if n > 0 {
+		tmp := make([]Byte, n)
+		_, _ = b.Read(tmp)
+		return Value{n, tmp}
+	} else {
+		return Value{N: n}
+	}
+}
+
+// ReadInet reads and returns Inet from the buffer.
+func ReadInet(b *bytes.Buffer) Inet {
+	// Reads length of the IP address.
+	n := ReadByte(b)
+	// Checks for valid length of the IP address.
+	if n != 4 && n != 16 {
+		panic(invalidIPLength)
+	}
+	// Reads IP address.
+	tmp := make([]Byte, n)
+	_, _ = b.Read(tmp)
+	return Inet{tmp, ReadInt(b)}
 }
 
 // ReadConsistency reads Short if it is valid consistency
@@ -160,7 +247,17 @@ func ReadWriteType(b *bytes.Buffer) string {
 func ReadString(b *bytes.Buffer) string {
 	// Reads length of the string.
 	n := ReadShort(b)
-	// Placeholder for read bytes.
+	// Placeholder for read Bytes.
+	tmp := make([]byte, n)
+	_, _ = b.Read(tmp)
+	return string(tmp)
+}
+
+// ReadLongString reads and returns string from the buffer.
+func ReadLongString(b *bytes.Buffer) string {
+	// Reads length of the long string.
+	n := ReadInt(b)
+	// Placeholder for read Bytes.
 	tmp := make([]byte, n)
 	_, _ = b.Read(tmp)
 	return string(tmp)
---
+++ frame/request/query.go
@@ -0,0 +1,64 @@
+package request
+
+import (
+	"bytes"
+	"scylla-go-driver/frame"
+)
+
+// Flags used inside Query.
+const (
+	values            = 0x01
+	skipMetadata      = 0x02
+	pageSize          = 0x04
+	pagingState       = 0x08
+	serialConsistency = 0x10
+	timestamp         = 0x20
+	namedValues       = 0x40
+)
+
+// Query request type message.
+// Consists of values required for all flags.
+// Values for unset flags are uninitialized.
+type Query struct {
+	Query             string
+	Consistency       frame.Short
+	Flags             frame.Byte
+	Values            []frame.Value
+	Names             []string
+	PageSize          frame.Int
+	PagingState       frame.Bytes
+	SerialConsistency frame.Short
+	Timestamp         frame.Long
+}
+
+// Write writes Query to the buffer.
+func (q Query) Write(b *bytes.Buffer) {
+	frame.WriteLongString(q.Query, b)
+	frame.WriteShort(q.Consistency, b)
+	frame.WriteByte(q.Flags, b)
+	// Checks the flags and writes values correspondent to the ones that are set.
+	if values&q.Flags != 0 {
+		if namedValues&q.Flags != 0 {
+			for i := range q.Names {
+				frame.WriteString(q.Names[i], b)
+				frame.WriteValue(q.Values[i], b)
+			}
+		} else {
+			for _, v := range q.Values {
+				frame.WriteValue(v, b)
+			}
+		}
+	}
+	if pageSize&q.Flags != 0 {
+		frame.WriteInt(q.PageSize, b)
+	}
+	if pagingState&q.Flags != 0 {
+		frame.WriteBytes(q.PagingState, b)
+	}
+	if serialConsistency&q.Flags != 0 {
+		frame.WriteShort(q.SerialConsistency, b)
+	}
+	if timestamp&q.Flags != 0 {
+		frame.WriteLong(q.Timestamp, b)
+	}
+}
