501b33b124b71103cefe8fbef8cf9b601fdad0a2
Hubert Grochowski <hubert.grochowski21@gmail.com>
2021-11-17 14:25:17 +0100

    Execute: WriteTo bytes.Buffer.

--- frame/request/execute.go
+++ frame/request/execute.go
@@ -1,7 +1,17 @@
 package request
 
-import "scylla-go-driver/frame"
+import (
+	"bytes"
+	"scylla-go-driver/frame"
+)
 
 type Execute struct {
-	id frame.Bytes
+	ID frame.Bytes
+	Options QueryOptions
+}
+
+// WriteTo writes Query ID and QueryOptions into the bytes.Buffer.
+func (e Execute) WriteTo (b *bytes.Buffer) {
+	_, _ = b.Write(e.ID)
+	e.Options.WriteTo(b)
 }
