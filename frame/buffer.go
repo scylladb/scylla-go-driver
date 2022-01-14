3faeca05c4afacbe4686d452276a99dfbad9c9eb
Michał Matczuk <michal@scylladb.com>
2022-01-14 13:30:47 +0100

    frame: buffer, add CopyBuffer

--- frame/buffer.go
+++ frame/buffer.go
@@ -2,7 +2,13 @@ package frame
 
 import (
 	"bytes"
+	"io"
 )
+
+// CopyBuffer writes data to w until the buffer is drained or an error occurs.
+func CopyBuffer(b *Buffer, w io.Writer) (n int64, err error) {
+	return b.buf.WriteTo(w)
+}
 
 type Buffer struct {
 	buf     bytes.Buffer
