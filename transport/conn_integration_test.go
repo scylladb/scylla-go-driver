da89bafaf64a8da8bf027f211de2e82680c76a1f
Michal-Leszczynski <2000michal@wp.pl>
2022-01-23 17:50:20 +0100

    Transport: added OpenConn function family.
    There are three functions in OpenConn family:
    - OpenShardConn - connects to a specific shard
    - OpenLocalPortConn - connects from a specific local port
    - OpenConn - connects from a specific local address
    Moreover, added simple functions for choosing desired local port.
    
    Added shard aware port (19042) for docker-compose.
    There is an issue with choosin local IP when connecting with shard aware policy
    that is described in code comment.
    
    Fixes #104

--- transport/conn_integration_test.go
+++ transport/conn_integration_test.go
@@ -3,19 +3,29 @@
 package transport
 
 import (
-	"net"
-	"testing"
-
 	"scylla-go-driver/frame"
+	"testing"
 )
 
 func TestConnStartup(t *testing.T) {
-	nc, err := net.Dial("tcp", "localhost:9042")
+	//nc, err := net.Dial("tcp", "localhost:9042")
+	//if err != nil {
+	//	t.Fatal(err)
+	//}
+	//conn := WrapConn(nc, TestStreamIDAllocator{})
+
+	si := ShardInfo{
+		Shard:    1,
+		NrShards: 2, // Note that scylla node from docker-compose has only 2 shards.
+	}
+	// Similar problem as in OpenLocalPortConn where only some forms of local IP works fine with
+	// shard aware policy. I tested it manually using time.sleep() and checking if connection was
+	// mapped to appropriate shard with cqlsh ("SELECT * FROM system.clients;").
+	// Here only 172.19.0.2 IP ensures correct shard mapping.
+	conn, err := OpenShardConn("172.19.0.2:19042", si, ConnConfig{}, TestStreamIDAllocator{})
 	if err != nil {
 		t.Fatal(err)
 	}
-	conn := WrapConn(nc)
-
 	opts := frame.StartupOptions{
 		"CQL_VERSION": "3.0.0",
 	}
