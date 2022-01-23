e640d9ab57d41d378de8cf2b28f55ee74af5ccd1
Michal-Leszczynski <2000michal@wp.pl>
2022-01-23 17:50:20 +0100

    Transport: added OpenConn function family.
    There are three functions in OpenConn family:
    - OpenShardConn - connects to a specific shard
    - OpenLocalPortConn - connects from a specific local port
    - OpenConn - connects from a specific local address
    Moreover, added simple functions for choosing desired local port.
    
    Added shard aware port (19042) for docker-compose.
    There is an issue with choosin local IP when connecting with shard aware policy
    that is described in code comment.
    
    Fixes #104

--- transport/conn.go
+++ transport/conn.go
@@ -8,7 +8,9 @@ import (
 	"log"
 	"net"
 	"runtime"
+	"strconv"
 	"sync"
+	"time"
 
 	"scylla-go-driver/frame"
 	. "scylla-go-driver/frame/request"
@@ -174,6 +176,8 @@ func (c *connReader) parse(op frame.OpCode) frame.Response {
 		return ParseError(&c.buf)
 	case frame.OpReady:
 		return ParseReady(&c.buf)
+	case frame.OpSupported:
+		return ParseSupported(&c.buf)
 	default:
 		log.Fatalf("not supported %d", op)
 		return nil
@@ -186,10 +190,63 @@ type Conn struct {
 	r    connReader
 }
 
+type ConnConfig struct {
+	TCPNoDelay bool
+	Timeout    time.Duration
+	// This will be used.
+	// DefaultConsistency frame.Consistency
+}
+
 const (
 	requestChanSize = 1024
 	ioBufferSize    = 8192
 )
+
+// OpenShardConn opens connection mapped to a specific shard on scylla node.
+func OpenShardConn(addr string, si ShardInfo, cfg ConnConfig) (*Conn, error) { // nolint:unused // This will be used.
+	it := ShardPortIterator(si)
+	maxTries := (maxPort-minPort+1)/int(si.NrShards) + 1
+	for i := 0; i < maxTries; i++ {
+		if conn, err := OpenLocalPortConn(addr, it(), cfg); err == nil {
+			return conn, nil
+		}
+	}
+
+	return nil, fmt.Errorf("failed to open connection on shard port: all local ports are busy")
+}
+
+// OpenLocalPortConn opens connection on a given local port.
+func OpenLocalPortConn(addr string, localPort uint16, cfg ConnConfig) (*Conn, error) {
+	// Not sure about local IP address. Empty IP and 172.19.0.1 works fine during tests but localhost does not.
+	// The problem is that when using localhost as IP connections are not mapped for appropriate shards
+	// even when using shard aware policy.
+	localAddr, err := net.ResolveTCPAddr("tcp", ":"+strconv.Itoa(int(localPort)))
+	if err != nil {
+		return nil, fmt.Errorf("resolving local TCP address: %w", err)
+	}
+
+	return OpenConn(addr, localAddr, cfg)
+}
+
+// OpenConn opens connection with specific local address.
+// In case lAddr is nil, random local address is chosen.
+func OpenConn(addr string, localAddr *net.TCPAddr, cfg ConnConfig) (*Conn, error) {
+	d := net.Dialer{
+		Timeout:   cfg.Timeout,
+		LocalAddr: localAddr,
+	}
+	conn, err := d.Dial("tcp", addr)
+	if err != nil {
+		return nil, fmt.Errorf("dialing TCP address %s: %w", addr, err)
+	}
+
+	tcpConn := conn.(*net.TCPConn)
+	if err = tcpConn.SetNoDelay(cfg.TCPNoDelay); err != nil {
+		return nil, fmt.Errorf("setting TCP no delay option: %w", err)
+	}
+
+	return WrapConn(tcpConn), nil
+}
 
 func WrapConn(conn net.Conn) *Conn {
 	c := &Conn{
---
+++ transport/routing.go
@@ -0,0 +1,39 @@
+package transport
+
+import "math/rand"
+
+const (
+	// Range of ports that can be used to establish connection.
+	maxPort = 65535
+	minPort = 49152
+)
+
+type ShardInfo struct {
+	Shard    uint16
+	NrShards uint16
+	// This will be used.
+	// MsbIgnore uint8
+}
+
+// RandomShardPort returns randomly generated port that can be used
+// to establish connection to a specific shard on scylla node.
+func RandomShardPort(si ShardInfo) uint16 {
+	maxRange := int(maxPort - si.NrShards + 1)
+	minRange := int(minPort + si.NrShards - 1)
+	r := uint16(rand.Intn(maxRange-minRange+1) + minRange)
+	return r/si.NrShards*si.NrShards + si.Shard
+}
+
+// ShardPortIterator returns iterator for consecutive ports that are
+// mapped to a specific shard on scylla node.
+func ShardPortIterator(si ShardInfo) func() uint16 {
+	port := RandomShardPort(si)
+
+	return func() uint16 {
+		port += si.NrShards
+		if port > maxPort {
+			port = (minPort+si.NrShards-1)/si.NrShards*si.NrShards + si.Shard
+		}
+		return port
+	}
+}
