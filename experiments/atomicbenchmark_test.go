89b4bd7abfd33392ce8fd658ec65d03372ce86ba
Hubert Grochowski <hubert.grochowski21@gmail.com>
2022-02-24 13:10:46 +0100

    experiments: benchmark of atomic - value vs unsafe pointer vs uber package
    goos: darwin
    goarch: amd64
    pkg: scylla-go-driver/experiments
    cpu: Intel(R) Core(TM) i7-7700HQ CPU @ 2.80GHz
    BenchmarkLoadAV
    BenchmarkLoadAV-8       	864799339	         1.216 ns/op
    BenchmarkLoadUP
    BenchmarkLoadUP-8       	1000000000	         0.4040 ns/op
    BenchmarkLoadUberAV
    BenchmarkLoadUberAV-8   	946084138	         1.198 ns/op
    BenchmarkLoadUberUP
    BenchmarkLoadUberUP-8   	1000000000	         0.5388 ns/op
    PASS

--- experiments/atomicbenchmark_test.go
+++ experiments/atomicbenchmark_test.go
@@ -4,6 +4,8 @@ import (
 	"sync/atomic"
 	"testing"
 	"unsafe"
+
+	uber "go.uber.org/atomic"
 )
 
 type data = map[string]int
@@ -34,6 +36,30 @@ func (u *up) load() data {
 	return *(*data)(atomic.LoadPointer(&u.v))
 }
 
+type uberUP struct {
+	v uber.UnsafePointer
+}
+
+func (u *uberUP) store(m data) {
+	u.v.Store(unsafe.Pointer(&m))
+}
+
+func (u *uberUP) load() data {
+	return *(*data)(u.v.Load())
+}
+
+type uberAV struct {
+	v uber.Value
+}
+
+func (u *uberAV) store(m data) {
+	u.v.Store(m)
+}
+
+func (u *uberAV) load() data {
+	return u.v.Load().(data)
+}
+
 func BenchmarkLoadAV(b *testing.B) {
 	var a av
 	d := make(data)
@@ -51,6 +77,36 @@ func BenchmarkLoadAV(b *testing.B) {
 
 func BenchmarkLoadUP(b *testing.B) {
 	var a up
+	d := make(data)
+	a.store(d)
+
+	var temp data
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		temp = a.load()
+	}
+
+	atomicResult = temp
+}
+
+func BenchmarkLoadUberAV(b *testing.B) {
+	var a uberAV
+	d := make(data)
+	a.store(d)
+
+	var temp data
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		temp = a.load()
+	}
+
+	atomicResult = temp
+}
+
+func BenchmarkLoadUberUP(b *testing.B) {
+	var a uberUP
 	d := make(data)
 	a.store(d)
 
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module scylla-go-driver
 
 go 1.17
 
-require github.com/google/go-cmp v0.5.6
+require (
+	github.com/google/go-cmp v0.5.6
+	go.uber.org/atomic v1.9.0
+)
