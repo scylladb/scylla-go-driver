88e64bb32b4dd293cdaf3584ac4d795d9329a521
Neofine <delavock@gmail.com>
2022-03-05 15:25:52 +0000

    pool: remove useless tests
    - Added constant to improve readability
    
    Fixes #135

--- transport/pool_integration_test.go
+++ transport/pool_integration_test.go
@@ -7,6 +7,8 @@ import (
 	"time"
 )
 
+const refillerBackoff = 250 * time.Millisecond
+
 func TestNodeConnPoolIntegration(t *testing.T) {
 	p, err := NewConnPool(TestHost+":9042", ConnConfig{})
 	if err != nil {
@@ -14,7 +16,7 @@ func TestNodeConnPoolIntegration(t *testing.T) {
 	}
 
 	t.Log("Wait for refiller to fill connections to shards")
-	time.Sleep(250 * time.Millisecond)
+	time.Sleep(refillerBackoff)
 
 	t.Log("Check if connections to shards are established")
 	for i, c := range p.AllConns() {
@@ -29,7 +31,7 @@ func TestNodeConnPoolIntegration(t *testing.T) {
 	}
 
 	t.Log("Wait for refiller to fill connections to shards")
-	time.Sleep(250 * time.Millisecond)
+	time.Sleep(refillerBackoff)
 
 	t.Log("Check if connections have been refilled")
 	for i, c := range p.AllConns() {
@@ -40,7 +42,7 @@ func TestNodeConnPoolIntegration(t *testing.T) {
 
 	t.Log("Close pool")
 	p.Close()
-	time.Sleep(250 * time.Millisecond)
+	time.Sleep(refillerBackoff)
 
 	t.Log("Check if connections are closed")
 	for _, c := range p.AllConns() {
@@ -49,78 +51,3 @@ func TestNodeConnPoolIntegration(t *testing.T) {
 		}
 	}
 }
-
-//func TestCloseConnPoolIntegration(t *testing.T) {
-//	cp := NewConnPool(TestHost+":19042", ConnConfig{})
-//	if cp == nil {
-//		t.Fatal("Couldn't start first connection.")
-//	}
-//
-//	time.Sleep(2 * time.Second)
-//
-//	go func() {
-//		for idx := range cp.conns {
-//			if conn := cp.loadConn(uint16(idx)); conn != nil {
-//				conn.Close()
-//			}
-//		}
-//	}()
-//
-//	cp.Close()
-//
-//	time.Sleep(10 * time.Millisecond)
-//
-//	for idx := range cp.conns {
-//		if conn := cp.loadConn(uint16(idx)); conn != nil {
-//			t.Fatal("Not all connections have been closed.")
-//		}
-//	}
-//}
-//
-//func TestConnPoolConnIntegration(t *testing.T) {
-//	cp := NewConnPool(TestHost+":19042", ConnConfig{})
-//	if cp == nil {
-//		t.Fatal("Couldn't start first connection.")
-//	}
-//
-//	if cp.NextConn() == nil {
-//		t.Fatal("NextConn returns nil even though initConnPool() has been successful.")
-//	}
-//
-//	time.Sleep(2 * time.Second)
-//
-//	murmurToken := MurmurToken([]byte(""))
-//
-//	if conn := cp.Conn(murmurToken); conn == nil || conn.Shard() != 0 {
-//		t.Fatal("Invalid return of Conn.")
-//	}
-//
-//	murmurToken = MurmurToken([]byte("0")) // Very big number approx. 3 * 10^18.
-//
-//	if conn := cp.Conn(murmurToken); conn == nil {
-//		t.Fatal("Invalid return of Conn.")
-//	}
-//
-//	go func() {
-//		seenNil := false
-//		ticker := time.NewTicker(1 * time.Second)
-//		for {
-//			select {
-//			case _ = <-ticker.C:
-//				return
-//			default:
-//				conn := cp.RandConn()
-//				if conn == nil {
-//					seenNil = true
-//				} else if seenNil == true {
-//					t.Fatal("Inconsistent RandConn return.")
-//				}
-//			}
-//		}
-//	}()
-//
-//	cp.Close()
-//
-//	// Waiting for go func().
-//	time.Sleep(1 * time.Second)
-//}
