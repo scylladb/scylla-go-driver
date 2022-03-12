55cde31f6b66d770a01548e69a1da2e2c6eba583
Neofine <delavock@gmail.com>
2022-03-12 15:15:40 +0000

    pool: replaced RandomConn and NextConn with LeastBusyConn
    Inspired by https://github.com/scylladb/gocql/pull/86
    In comparison to PR above our code doesn't use round robin in leastBusyConn()
    Added simple test assuring load distribution to single connection works
    
    Fixes #137

--- transport/pool_integration_test.go
+++ transport/pool_integration_test.go
@@ -3,6 +3,7 @@
 package transport
 
 import (
+	"math"
 	"testing"
 	"time"
 )
@@ -50,4 +51,42 @@ func TestNodeConnPoolIntegration(t *testing.T) {
 			t.Fatalf("conn %s", c)
 		}
 	}
+}
+
+func TestConnPoolConn(t *testing.T) {
+	p, err := NewConnPool(TestHost+":9042", ConnConfig{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("Wait for refiller to fill connections to shards")
+	time.Sleep(refillerBackoff)
+
+	t.Log("Check if connections to shards are established")
+	for i, c := range p.AllConns() {
+		if c == nil {
+			t.Fatalf("no conn for shard %d", i)
+		}
+	}
+
+	murmurToken := MurmurToken([]byte(""))
+	if conn := p.Conn(murmurToken); conn == nil || conn.Shard() != 0 {
+		t.Fatal("invalid return of Conn")
+	}
+
+	load := uint32(math.Floor(maxStreamID*heavyLoadThreshold + 1))
+	p.Conn(murmurToken).metrics.InQueue.Store(load)
+	conn := p.Conn(murmurToken)
+	if conn == nil {
+		t.Fatal("invalid return of Conn")
+	} else if conn.Shard() == 0 {
+		t.Fatalf("invalid load distribution")
+	}
+
+	murmurToken = MurmurToken([]byte("0")) // Very big number approx. 3 * 10^18.
+	if conn := p.Conn(murmurToken); conn == nil {
+		t.Fatal("invalid return of Conn")
+	}
+
+	p.Close()
 }
