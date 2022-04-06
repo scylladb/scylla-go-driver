8877b5f9fcd623baf14f4d03525794157e26c267
Hubert Grochowski <hubert.grochowski21@gmail.com>
2022-04-06 13:11:39 +0200

    session: config reformat
    - aliased frame types
    - added validate method with proper error msg
    - added copy method to make config immutable

--- session.go
+++ session.go
@@ -2,8 +2,8 @@ package scylla
 
 import (
 	"fmt"
+	"time"
 
-	"github.com/mmatczuk/scylla-go-driver/frame"
 	"github.com/mmatczuk/scylla-go-driver/transport"
 )
 
@@ -11,11 +11,98 @@ import (
 // TODO: Add Query Paging.
 // TODO: Merge with host selection policy.
 
+type EventType = string
+
+const (
+	TopologyChange EventType = "TOPOLOGY_CHANGE"
+	StatusChange   EventType = "STATUS_CHANGE"
+	SchemaChange   EventType = "SCHEMA_CHANGE"
+)
+
+type Consistency = uint16
+
+const (
+	ANY         Consistency = 0x0000
+	ONE         Consistency = 0x0001
+	TWO         Consistency = 0x0002
+	THREE       Consistency = 0x0003
+	QUORUM      Consistency = 0x0004
+	ALL         Consistency = 0x0005
+	LOCALQUORUM Consistency = 0x0006
+	EACHQUORUM  Consistency = 0x0007
+	SERIAL      Consistency = 0x0008
+	LOCALSERIAL Consistency = 0x0009
+	LOCALONE    Consistency = 0x000A
+)
+
+var (
+	ErrNoHosts   = fmt.Errorf("error in session config: no hosts given")
+	ErrEventType = fmt.Errorf("error in session config: invalid event\npossible events:\n" +
+		"TopologyChange EventType = \"TOPOLOGY_CHANGE\"\n" +
+		"StatusChange   EventType = \"STATUS_CHANGE\"\n" +
+		"SchemaChange   EventType = \"SCHEMA_CHANGE\"")
+	ErrConsistency = fmt.Errorf("error in session config: invalid consistency\npossible consistencies are:\n" +
+		"ANY         Consistency = 0x0000\n" +
+		"ONE         Consistency = 0x0001\n" +
+		"TWO         Consistency = 0x0002\n" +
+		"THREE       Consistency = 0x0003\n" +
+		"QUORUM      Consistency = 0x0004\n" +
+		"ALL         Consistency = 0x0005\n" +
+		"LOCALQUORUM Consistency = 0x0006\n" +
+		"EACHQUORUM  Consistency = 0x0007\n" +
+		"SERIAL      Consistency = 0x0008\n" +
+		"LOCALSERIAL Consistency = 0x0009\n" +
+		"LOCALONE    Consistency = 0x000A")
+)
+
 type SessionConfig struct {
-	Hosts    []string
-	Keyspace string
-	Events   []frame.EventType
-	transport.ConnConfig
+	Hosts              []string
+	Keyspace           string
+	Events             []EventType
+	TCPNoDelay         bool
+	ConnectionTimeout  time.Duration
+	DefaultConsistency Consistency
+}
+
+func (s *SessionConfig) copy() *SessionConfig {
+	var cfgCopy SessionConfig
+
+	cfgCopy.Hosts = make([]string, len(s.Hosts))
+	copy(cfgCopy.Hosts, s.Hosts)
+
+	cfgCopy.Keyspace = s.Keyspace
+
+	cfgCopy.Events = make([]string, len(s.Events))
+	copy(cfgCopy.Events, s.Events)
+
+	cfgCopy.TCPNoDelay = s.TCPNoDelay
+	cfgCopy.ConnectionTimeout = s.ConnectionTimeout
+	cfgCopy.DefaultConsistency = s.DefaultConsistency
+
+	return &cfgCopy
+}
+
+func (s *SessionConfig) validate() error {
+	if len(s.Hosts) == 0 {
+		return ErrNoHosts
+	}
+
+	for _, e := range s.Events {
+		if e != TopologyChange && e != StatusChange && e != SchemaChange {
+			return ErrEventType
+		}
+	}
+
+	if s.DefaultConsistency > LOCALONE {
+		return ErrConsistency
+	}
+
+	// Use default timeout if not set.
+	if s.ConnectionTimeout == 0 {
+		s.ConnectionTimeout = time.Second
+	}
+
+	return nil
 }
 
 type Session struct {
@@ -23,8 +110,19 @@ type Session struct {
 	cluster *transport.Cluster
 }
 
-func NewSession(cfg *SessionConfig) (*Session, error) {
-	cluster, err := transport.NewCluster(cfg.ConnConfig, cfg.Events, cfg.Hosts...)
+func NewSession(config *SessionConfig) (*Session, error) {
+	cfg := config.copy()
+	if err := cfg.validate(); err != nil {
+		return nil, err
+	}
+
+	connCfg := transport.ConnConfig{
+		TCPNoDelay:         cfg.TCPNoDelay,
+		Timeout:            cfg.ConnectionTimeout,
+		DefaultConsistency: cfg.DefaultConsistency,
+	}
+
+	cluster, err := transport.NewCluster(connCfg, cfg.Events, cfg.Hosts...)
 	if err != nil {
 		return nil, err
 	}
