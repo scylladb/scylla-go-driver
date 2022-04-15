4fdcef86bd092cb1ecae9ea894a3c1dffd3de8fe
Michał Matczuk <michal@scylladb.com>
2022-04-15 16:46:35 +0200

    experiments/cmd/benchtab: use async query
    This doubles performance
    
    ```
     michal@lh:Scylla/scylla-go-driver (mmt/try_again)$ make run-benchtab
    2022/04/15 14:47:13 cluster: open control connection
    2022/04/15 14:47:13 [addr=192.168.100.100:9042 shard=0] connected
    2022/04/15 14:47:13 cluster: refresh topology
    2022/04/15 14:47:13 [addr=192.168.100.100:9042 shard=1] connected
    2022/04/15 14:47:13 [addr=192.168.100.100:19042 shard=0] connected
    2022/04/15 14:47:13 [addr=192.168.100.100:19042 shard=2] connected
    2022/04/15 14:47:13 [addr=192.168.100.100:19042 shard=3] connected
    2022/04/15 14:47:14 cluster: open control connection
    2022/04/15 14:47:14 [addr=192.168.100.100:9042 shard=2] connected
    2022/04/15 14:47:14 cluster: refresh topology
    2022/04/15 14:47:14 [addr=192.168.100.100:9042 shard=3] connected
    2022/04/15 14:47:14 Starting the benchmark
    2022/04/15 14:47:14 [addr=192.168.100.100:19042 shard=0] connected
    2022/04/15 14:47:14 [addr=192.168.100.100:19042 shard=1] connected
    2022/04/15 14:47:14 [addr=192.168.100.100:19042 shard=2] connected
    2022/04/15 14:47:18 Finished
    Benchmark time: 4206 ms
    ```

--- experiments/cmd/benchtab/main.go
+++ experiments/cmd/benchtab/main.go
@@ -34,66 +34,61 @@ func main() {
 		initSelectsBenchmark(session, config)
 	}
 
-	var wg sync.WaitGroup
-	nextBatchStart := int64(0)
-
 	log.Println("Starting the benchmark")
-
 	startTime := time.Now()
 
-	for i := int64(0); i < config.concurrency; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			insertQ, err := session.Prepare(insertStmt)
-			if err != nil {
-				log.Fatal(err)
-			}
-			selectQ, err := session.Prepare(selectStmt)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			for {
-				curBatchStart := atomic.AddInt64(&nextBatchStart, config.batchSize)
-				if curBatchStart >= config.tasks {
-					// no more work to do
-					break
-				}
-
-				curBatchEnd := min(curBatchStart+config.batchSize, config.tasks)
-
-				for pk := curBatchStart; pk < curBatchEnd; pk++ {
-					if config.workload == Inserts || config.workload == Mixed {
-						insertQ.BindInt64(0, pk)
-						insertQ.BindInt64(1, 2*pk)
-						insertQ.BindInt64(2, 3*pk)
-						if _, err := insertQ.Exec(); err != nil {
-							log.Fatal(err)
-						}
-					}
-
-					if config.workload == Selects || config.workload == Mixed {
-						selectQ.BindInt64(0, pk)
-						res, err := selectQ.Exec()
-						if err != nil {
-							log.Fatal(err)
-						}
-
-						v1, _ := res.Rows[0][0].AsInt64()
-						v2, _ := res.Rows[0][1].AsInt64()
-						if v1 != 2*pk || v2 != 3*pk {
-							log.Fatalf("expected (%d, %d), got (%d, %d)", 2*pk, 3*pk, v1, v2)
-						}
-					}
-				}
-			}
-		}()
+	insertQ, err := session.Prepare(insertStmt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	selectQ, err := session.Prepare(selectStmt)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	wg.Wait()
-	benchTime := time.Now().Sub(startTime)
+	if config.workload == Inserts || config.workload == Mixed {
+		for pk := int64(0); pk < config.tasks; pk++ {
+			insertQ.BindInt64(0, pk)
+			insertQ.BindInt64(1, 2*pk)
+			insertQ.BindInt64(2, 3*pk)
+			insertQ.AsyncExec()
+		}
+		for pk := int64(0); pk < config.tasks; pk++ {
+			if _, err := insertQ.Fetch(); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+	if config.workload == Selects || config.workload == Mixed {
+		for pk := int64(0); pk < config.tasks; pk++ {
+			selectQ.BindInt64(0, pk)
+			selectQ.AsyncExec()
+		}
+		for pk := int64(0); pk < config.tasks; pk++ {
+			res, err := selectQ.Fetch()
+			if err != nil {
+				log.Fatal(err)
+			}
 
+			if len(res.Rows) != 1 {
+				log.Fatalf("expected 1 row, got %d", len(res.Rows))
+			}
+
+			v1, err := res.Rows[0][0].AsInt64()
+			if err != nil {
+				log.Fatal(err)
+			}
+			v2, err := res.Rows[0][1].AsInt64()
+			if err != nil {
+				log.Fatal(err)
+			}
+			if v1 != 2*pk || v2 != 3*pk {
+				log.Fatalf("expected (%d, %d), got (%d, %d)", 2*pk, 3*pk, v1, v2)
+			}
+		}
+	}
+
+	benchTime := time.Now().Sub(startTime)
 	log.Printf("Finished\nBenchmark time: %d ms\n", benchTime.Milliseconds())
 }
 
