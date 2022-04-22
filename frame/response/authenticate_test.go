a089a070b006d3f638f33adddc4603f6788c630a
Michał Matczuk <michal@scylladb.com>
2022-04-22 12:31:53 +0200

    frame: remove void test cases

--- frame/response/authenticate_test.go
+++ frame/response/authenticate_test.go
@@ -43,10 +43,6 @@ var dummyA *Authenticate
 // We want to make sure that parsing does not crush driver even for random data.
 // We assign result to global variable to avoid compiler optimization.
 func FuzzAuthenticate(f *testing.F) {
-	testCases := [][]byte{make([]byte, 1000000)}
-	for _, tc := range testCases {
-		f.Add(tc)
-	}
 	f.Fuzz(func(t *testing.T, data []byte) { // nolint:thelper // This is not a helper function.
 		var buf frame.Buffer
 		buf.Write(data)
