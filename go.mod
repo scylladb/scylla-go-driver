3c89450e0b20abee164bc29cc430730a3397af41
Paweł Putra <pawel.putra@scylladb.com>
2022-08-31 15:30:22 +0200

    transport: pool, store connections using atomic.Pointer[Conn]

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/google/go-cmp v0.5.6
 	github.com/klauspost/compress v1.15.1
 	github.com/pierrec/lz4/v4 v4.1.14
-	go.uber.org/atomic v1.9.0
+	go.uber.org/atomic v1.10.0
 	go.uber.org/goleak v1.1.12
 )
 
--- transport/export_test.go
+++ transport/export_test.go
@@ -2,8 +2,8 @@ package transport
 
 func (p *ConnPool) AllConns() []*Conn {
 	var conns = make([]*Conn, len(p.conns))
-	for i, v := range p.conns {
-		conns[i], _ = v.Load().(*Conn)
+	for i := range conns {
+		conns[i] = p.loadConn(i)
 	}
 	return conns
 }
